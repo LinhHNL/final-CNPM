1189b8d025eee4901095c73e42e94b869a1f958f
Huỳnh Nhật Linh <92385756+LinhHNL@users.noreply.github.com>
2023-03-04 11:43:26 +0700

    finish noi lau

--- HomepageFormSingle.cs
+++ HomepageFormSingle.cs
@@ -47,6 +47,88 @@ namespace WinFormsApp2
 
         }
 
+        private void btn_singlePot_Click(object sender, EventArgs e)
+        {
+            panel_doublePot.Visible = false;
+            panel_singlePot.Visible = true;
+            panel_fourPot.Visible = false;
+            btn_singlePot.BackColor = ColorTranslator.FromHtml("#E50111");
+            btn_singlePot.ForeColor = Color.White;
+            btn_doublePot.BackColor = ColorTranslator.FromHtml("#DEE2E6");
+            btn_doublePot.ForeColor = ColorTranslator.FromHtml("#A09F9F");
+            btn_fourPot.BackColor = ColorTranslator.FromHtml("#DEE2E6");
+            btn_fourPot.ForeColor = ColorTranslator.FromHtml("#A09F9F");
+        }
 
+        private void btn_doublePot_Click(object sender, EventArgs e)
+        {
+            panel_doublePot.Visible = true;
+            panel_singlePot.Visible = false;
+            panel_fourPot.Visible = false;
+            btn_doublePot.BackColor = ColorTranslator.FromHtml("#E50111");
+            btn_doublePot.ForeColor = Color.White;
+            btn_singlePot.BackColor = ColorTranslator.FromHtml("#DEE2E6");
+            btn_singlePot.ForeColor = ColorTranslator.FromHtml("#A09F9F");
+            btn_fourPot.BackColor = ColorTranslator.FromHtml("#DEE2E6");
+            btn_fourPot.ForeColor = ColorTranslator.FromHtml("#A09F9F");
+        }
+
+        private void btn_fourPot_Click(object sender, EventArgs e)
+        {
+            panel_doublePot.Visible = false;
+            panel_singlePot.Visible = false;
+            panel_fourPot.Visible = true;
+            btn_fourPot.BackColor = ColorTranslator.FromHtml("#E50111");
+            btn_fourPot.ForeColor = Color.White;
+            btn_singlePot.BackColor = ColorTranslator.FromHtml("#DEE2E6");
+            btn_singlePot.ForeColor = ColorTranslator.FromHtml("#A09F9F");
+            btn_doublePot.BackColor = ColorTranslator.FromHtml("#DEE2E6");
+            btn_doublePot.ForeColor = ColorTranslator.FromHtml("#A09F9F");
+        }
+
+        private void metroTile2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("");
+        }
+
+        private void roundedButton18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_rightDoublePot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_topLeftFourPot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_bottomLeftFourPot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_topRightFourPot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_bottomRightFourPot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void roundedButton22_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void roundedButton3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
