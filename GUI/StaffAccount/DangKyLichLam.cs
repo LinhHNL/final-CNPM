9c61b643f34eb9a2ce4171d9fce3b3a7272182f1
kiet15092003 <kieucaominhkiet15092003@gmail.com>
2023-05-06 22:05:01 +0700

    create language resx

---
+++ GUI/StaffAccount/DangKyLichLam.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2.StaffAccount
+{
+    public partial class DangKyLichLam : MetroFramework.Forms.MetroForm
+    {
+        int day;
+        int month;
+        int year;
+        public DangKyLichLam()
+        {
+            InitializeComponent();
+        }
+
+        public DangKyLichLam(int day, int month, int year)
+        {
+            InitializeComponent();
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+
+        private void DangKyLichLam_Load(object sender, EventArgs e)
+        {
+            int daysInCurrentMonth = DateTime.DaysInMonth(year, month);
+            for (int i = daysInCurrentMonth; i >= 1; i--)
+            {
+                cb_date.Items.Add(i + "/" + month + "/" + year);
+            }
+            cb_date.SelectedIndex = daysInCurrentMonth - day;
+
+        }
+
+        private void btn_CaSang_MouseEnter(object sender, EventArgs e)
+        {
+            lbl_CaSang.BackColor = ColorTranslator.FromHtml("#e5e5e5");
+        }
+
+        private void btn_CaSang_MouseLeave(object sender, EventArgs e)
+        {
+            lbl_CaSang.BackColor = Color.White;
+        }
+
+        private void btn_CaChieu_MouseEnter(object sender, EventArgs e)
+        {
+            lbl_CaChieu.BackColor = ColorTranslator.FromHtml("#e5e5e5");
+        }
+
+        private void btn_CaChieu_MouseLeave(object sender, EventArgs e)
+        {
+            lbl_CaChieu.BackColor = Color.White;
+        }
+
+        private void btn_CaToi_MouseEnter(object sender, EventArgs e)
+        {
+            lbl_CaToi.BackColor = ColorTranslator.FromHtml("#e5e5e5");
+        }
+
+        private void btn_CaToi_MouseLeave(object sender, EventArgs e)
+        {
+            lbl_CaToi.BackColor = Color.White;
+        }
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            LichLam form = new LichLam();
+            this.Hide();
+            form.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_CaSang_Click(object sender, EventArgs e)
+        {
+            //QuanLyLichLamCa form = new QuanLyLichLamCa(day, month, year, 0);
+            this.Hide();
+            //form.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_CaChieu_Click(object sender, EventArgs e)
+        {
+            //QuanLyLichLamCa form = new QuanLyLichLamCa(day, month, year, 1);
+            this.Hide();
+            //form.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_CaToi_Click(object sender, EventArgs e)
+        {
+            //QuanLyLichLamCa form = new QuanLyLichLamCa(day, month, year, 2);
+            this.Hide();
+            //form.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_QuanLyLichLam_Click(object sender, EventArgs e)
+        {
+            //QuanLyLichLam form = new QuanLyLichLam();
+            this.Hide();
+            //form.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_QuanLyChamCong_Click(object sender, EventArgs e)
+        {
+            //QuanLyChamCong form = new QuanLyChamCong();
+            this.Hide();
+            //form.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_DonTu_Click(object sender, EventArgs e)
+        {
+            //QuanLyDonTu form = new QuanLyDonTu();
+            this.Hide();
+            //form.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_LuongThuong_Click(object sender, EventArgs e)
+        {
+            //QuanLyLuongThuong form = new QuanLyLuongThuong();
+            this.Hide();
+            //form.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_HoSoNhanVien_Click(object sender, EventArgs e)
+        {
+            //HoSoNhanVien form = new HoSoNhanVien();
+            this.Hide();
+            //form.ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ GUI/StaffAccount/LichLam.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinFormsApp2.CustomControls;
+
+namespace WinFormsApp2.StaffAccount
+{
+    public partial class LichLam : MetroFramework.Forms.MetroForm
+    {
+        public LichLam()
+        {
+            InitializeComponent();
+            calendar.cellClickEvent = (sender, e) =>
+            {
+                int day = Int32.Parse(((CalendarCell)sender).getDay().Text);
+                //QuanLyLichLamNgay form = new QuanLyLichLamNgay(day, calendar.Month, calendar.Year);
+                this.Hide();
+                //form.ShowDialog();
+                this.Close();
+            };
+        }
+
+        private void QuanLyLichLam_Load(object sender, EventArgs e)
+        {
+            calendar.setParentForm(this);
+            calendar.SetMonthYear(DateTime.Now.Month, DateTime.Now.Year);
+            cb_month.SelectedIndex = 0;
+        }
+
+        private void cb_month_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cb_month_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            int month = Int32.Parse(cb_month.SelectedItem.ToString().Split("/")[0]);
+            int year = Int32.Parse(cb_month.SelectedItem.ToString().Split("/")[1]);
+            calendar.SetMonthYear(month, year);
+            calendar.Reload();
+        }
+
+        private void btn_QuanLyLichLam_Click(object sender, EventArgs e)
+        {
+            //QuanLyLichLam form = new QuanLyLichLam();
+            this.Hide();
+            //form.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_QuanLyChamCong_Click(object sender, EventArgs e)
+        {
+            //QuanLyChamCong form = new QuanLyChamCong();
+            this.Hide();
+            //form.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_DonTu_Click(object sender, EventArgs e)
+        {
+            //QuanLyDonTu form = new QuanLyDonTu();
+            this.Hide();
+            //form.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_LuongThuong_Click(object sender, EventArgs e)
+        {
+            //QuanLyLuongThuong form = new QuanLyLuongThuong();
+            this.Hide();
+            //form.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_HoSoNhanVien_Click(object sender, EventArgs e)
+        {
+            //HoSoNhanVien form = new HoSoNhanVien();
+            this.Hide();
+            //form.ShowDialog();
+            this.Close();
+        }
+    }
+}
