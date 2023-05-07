e1719b907b754939191f8757dfa6e189001f535b
kiet15092003 <kieucaominhkiet15092003@gmail.com>
2023-05-07 22:47:26 +0700

    complete all NhanVienPage

--- GUI/NhanVienPage/ThongTinCoBanNhanVien_CongViec.cs
+++ GUI/NhanVienPage/ThongTinCoBanNhanVien_CongViec.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Resources;
+using System.Globalization;
+using WinFormsApp2.AdminPage;
 
 namespace WinFormsApp2.NhanVienPage
 {
     public partial class ThongTinCoBanNhanVien_CongViec : MetroFramework.Forms.MetroForm
     {
+        CultureInfo culture;
         public ThongTinCoBanNhanVien_CongViec()
         {
             InitializeComponent();
+            culture = CultureInfo.CurrentCulture;
+            SetLanguage("en-US");
         }
         
 
@@ -95,6 +101,38 @@ namespace WinFormsApp2.NhanVienPage
             this.Hide();
             form.ShowDialog();
             this.Close();
+        }
+        private void SetLanguage(string cultureName)
+        {
+            culture = CultureInfo.CreateSpecificCulture(cultureName);
+            ResourceManager rm = new
+                ResourceManager("GUI.Language.MyResource", typeof(AddFood).Assembly);
+            btn_Signout.Text = rm.GetString("signoutText", culture);
+            lbl_AccountName.Text = rm.GetString("accountNameText", culture);
+            btn_StaffProfile.Text = rm.GetString("staffProfileText", culture);
+            btn_WorkScheduleManagement.Text = rm.GetString("workScheduleManagementText", culture);
+            btn_TimekeepingManagement.Text = rm.GetString("timekeepingManagementText", culture);
+            btn_letter.Text = rm.GetString("letterText", culture);
+            btn_ExportFile.Text = rm.GetString("exportFileText", culture);
+            StartDate.HeaderText = rm.GetString("startDateText", culture);
+            btn_PayRoll.Text = rm.GetString("payrollText", culture);
+            Description.HeaderText = rm.GetString("descriptionText", culture);
+            Number.HeaderText = rm.GetString("numberText", culture);
+            Position.HeaderText = rm.GetString("positionText", culture);
+            EndDate.HeaderText = rm.GetString("endDateText", culture);
+            lbl_status.Text = rm.GetString("statusText", culture);
+            lbl_timekeepingCode.Text = rm.GetString("timekeepingCodeText", culture);
+            lbl_Position.Text = rm.GetString("positionText", culture);
+            lbl_Department.Text = rm.GetString("departmentText", culture);
+            lbl_startWorkDay.Text = rm.GetString("startWorkDayText", culture);
+            btn_profile.Text = rm.GetString("profileText", culture);
+            btn_socialInsurance.Text = rm.GetString("socialInsuranceText", culture);
+            lbl_staffInfoText.Text = rm.GetString("staffInfoText", culture);
+            btn_job.Text = rm.GetString("jobText", culture);
+            lbl_Back.Text = rm.GetString("backText", culture);
+            gb_WorkInfo.Text = rm.GetString("workInfoText", culture);
+            btn_Edit.Text = rm.GetString("editText", culture);
+            gb_WorkProcess.Text = rm.GetString("workProgressText", culture);
         }
     }
 }
--- GUI/NhanVienPage/XoaNhanVien.cs
+++ GUI/NhanVienPage/XoaNhanVien.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Resources;
+using System.Globalization;
+using WinFormsApp2.AdminPage;
 
 namespace WinFormsApp2.NhanVienPage
 {
     public partial class XoaNhanVien : MetroFramework.Forms.MetroForm
     {
+        CultureInfo culture;
         public XoaNhanVien()
         {
             InitializeComponent();
@@ -21,6 +25,8 @@ namespace WinFormsApp2.NhanVienPage
             dgv_NhanVien.Rows.Add(true, "0", "asd", "asd", "asd", "asd", "adsd");
             dgv_NhanVien.Rows.Add(true, "0", "asd", "asd", "asd", "asd", "adsd");
             dgv_NhanVien.Rows.Add(true, "0", "asd", "asd", "asd", "asd", "adsd");
+            culture = CultureInfo.CurrentCulture;
+            SetLanguage("en-US");
         }
 
         private void btn_Back_Click(object sender, EventArgs e)
@@ -69,6 +75,32 @@ namespace WinFormsApp2.NhanVienPage
             this.Hide();
             form.ShowDialog();
             this.Close();
+        }
+        private void SetLanguage(string cultureName)
+        {
+            culture = CultureInfo.CreateSpecificCulture(cultureName);
+            ResourceManager rm = new
+                ResourceManager("GUI.Language.MyResource", typeof(AddFood).Assembly);
+            btn_Signout.Text = rm.GetString("signoutText", culture);
+            lbl_AccountName.Text = rm.GetString("accountNameText", culture);
+            btn_StaffProfile.Text = rm.GetString("staffProfileText", culture);
+            btn_WorkScheduleManagement.Text = rm.GetString("workScheduleManagementText", culture);
+            btn_TimekeepingManagement.Text = rm.GetString("timekeepingManagementText", culture);
+            btn_letter.Text = rm.GetString("letterText", culture);
+            btn_PayRoll.Text = rm.GetString("payrollText", culture);
+            btn_Back.Text = rm.GetString("backText", culture);
+            lbl_staffList.Text = rm.GetString("staffListText", culture);
+            lbl_Search.Text = rm.GetString("searchText", culture);
+            tb_StaffID.PlaceholderText = rm.GetString("staffIDText", culture);
+            cb_Department.Text = rm.GetString("departmentText", culture);
+            btn_Delete.Text = rm.GetString("deleteText", culture);
+            staffID.HeaderText = rm.GetString("staffIDText", culture);
+            FullName.HeaderText = rm.GetString("fullNameText", culture);
+            department.HeaderText = rm.GetString("departmentText", culture);
+            Position.HeaderText = rm.GetString("positionText", culture);
+            startWorkDate2.HeaderText = rm.GetString("startWorkDayText2", culture);
+            status.HeaderText = rm.GetString("statusText", culture);
+            cb_Department.PromptText = rm.GetString("departmentText", culture);
         }
     }
 }
